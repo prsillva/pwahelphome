811bf631a393f0b215eecae7f05067a286240c1d
prsillva <prsillva@gmail.com>
2022-11-28 23:51:25 -0300

    Segundo Envio - HelpHome

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,23 @@
 from django.urls import path
 from . import views
-from .views import SearchResultsView, ProfileDetailView, SearchResultsPwaView, ProfileDetailViewPwa
+
+from django.urls import path
+from .views import *
 
 urlpatterns = [
 
-    
-    path('', views.pwa, name='pwa'),
+    path('', views.home, name='home'),
+    path('pwa', views.pwa, name='pwa'),
     path('procurar/', views.procurar, name='procurar'),
     path('accounts/profile/', views.profile, name='profile'),
     path('accounts/profilepwa/', views.profilepwa, name='profile_pwa'),
     path('search', SearchResultsView.as_view(), name='search_results'),
     path('pesquisa', SearchResultsPwaView.as_view(), name='search_results_pwa'),
     path('profile/<int:pk>', ProfileDetailView.as_view(), name='profile_detail'),
-    path('profilepwa/<int:pk>', ProfileDetailViewPwa.as_view(), name='profile_detail_pwa')
+    path('profilepwa/<int:pk>', ProfileDetailViewPwa.as_view(), name='profile_detail_pwa'),
+    path('logar_usuario', logar_usuario, name="logar_usuario"),
+    path('deslogar_usuario', deslogar_usuario, name="deslogar_usuario"),
+    path('alterar_senha/', alterar_senha, name='alterar_senha'),
+    path('cadastrar_usuario', cadastrar_usuario, name="cadastrar_usuario")
 
 ]
--- core/views.py
+++ core/views.py
@@ -9,6 +9,54 @@ from .forms import UserUpdateForm, ProfileUpdateForm
 from .models import Profile, Review
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+
+def cadastrar_usuario(request):
+    if request.method == "POST":
+        form_usuario = UserCreationForm(request.POST)
+        if form_usuario.is_valid():
+            form_usuario.save()
+            return redirect('home')
+    else:
+        form_usuario = UserCreationForm()
+    return render(request, 'cadastro_user.html', {'form_usuario': form_usuario})
+
+def logar_usuario(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        usuario = authenticate(request, username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('home')
+        else:
+            form_login = AuthenticationForm()
+    else:
+        form_login = AuthenticationForm()
+    return render(request, 'login_site.html', {'form_login': form_login})
+
+@login_required(login_url='/logar_usuario')
+def deslogar_usuario(request):
+    logout(request)
+    return redirect('home')
+
+@login_required(login_url='/logar_usuario')
+def alterar_senha(request):
+    if request.method == "POST":
+        form_senha = PasswordChangeForm(request.user, request.POST)
+        if form_senha.is_valid():
+            user = form_senha.save()
+            update_session_auth_hash(request, user)
+            return redirect('index')
+    else:
+        form_senha = PasswordChangeForm(request.user)
+    return render(request, 'alterar_senha.html', {'form_senha': form_senha})
+
+
 
 def home(request):
         usuario = Profile.objects.all()
